7e9f19c36661615beed8768874bde0e9cb0bce5d
John <johngc2010@hotmail.com>
2019-04-04 03:25:09 -0300

    Foda

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
-require("dotenv").config()
-const Telegram = require("telegraf/telegram")
-const Telegraf = require("telegraf")
-let chats = []
-bot = new Telegraf(process.env.BOT_TOKEN)
-telegram_api = new Telegram(process.env.BOT_TOKEN)
+require('dotenv').config()
+const Telegram = require('telegraf/telegram')
+const Telegraf = require('telegraf')
+const session = require('telegraf/session')
 
-bot.start((ctx)=> {
-  if (!chats.includes(ctx.chat.id)) {
-    chats.push(ctx.chat.id)
-    ctx.reply("Se preparem para a coluna SUPREMA!")
-    ctx.reply("POSTURA!")
-  } 
+let chats = new Set()
+let bot = new Telegraf(process.env.BOT_TOKEN)
+let telegramApi = new Telegram(process.env.BOT_TOKEN)
+
+bot.use(session())
+bot.start((ctx) => {
+  if (!chats.has(ctx.chat.id)) {
+    chats.add(ctx.chat.id)
+    ctx.reply('Se preparem para a coluna SUPREMA!')
+    ctx.reply(`em ${process.env.WARNING_INTERVAL / 60000} minutos eu boto pra quebrar!`)
+  }
 })
-bot.command("abort",(ctx) => {
-  if(chats.length > 0){
-    chats = chats.filter((id) => id != ctx.chat.id)
-    ctx.reply("mamae pq me abortastes 😥")
-}
+bot.command('abort', (ctx) => {
+  if (chats.has(ctx.chat.id)) {
+    ctx.reply('mamae pq me abortastes 😥')
+    chats.delete(ctx.chat.id)
+  }
+})
+bot.command('arrumei', (ctx) => {
+
 })
 bot.launch()
 
 setInterval(async () => {
-  chats.forEach(chat_id => {
-    telegram_api.sendMessage(chat_id, "POSTURA!")   
-  });
-}, process.env.WARNING_INTERVAL);
+  chats.forEach(chatId => {
+    telegramApi.sendMessage(chatId, 'POSTURA!')
+  })
+}, process.env.WARNING_INTERVAL)
