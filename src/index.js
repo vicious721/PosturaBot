d6aba060b8ead47b1ee258f06993b68c0b2b013a
John <johngc2010@hotmail.com>
2019-10-29 00:08:45 -0300

    Update counter

--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
 const app = require("express")();
-
-require('dotenv').config()
+let count = 0;
+require('dotenv').config();
 // const Telegram = require('telegraf/telegram')
-const Telegraf = require('telegraf')
-const session = require('telegraf/session')
+const Telegraf = require('telegraf');
+const session = require('telegraf/session');
 
 // let chats = new Set()
-let bot = new Telegraf(process.env.BOT_TOKEN)
+let bot = new Telegraf(process.env.BOT_TOKEN);
 // let telegramApi = new Telegram(process.env.BOT_TOKEN)
 
-const interval = process.env.WARNING_INTERVAL * 3600000
+const interval = process.env.WARNING_INTERVAL * 3600000;
 
 bot.use(session())
 bot.start((ctx) => {
-  if (!ctx.session.warning) {
+  if (!ctx.session.warning && count++ < 1) {
     // chats.add(ctx.chat.id)
-    ctx.reply('Se preparem para a coluna SUPREMA!')
-    ctx.reply(`em ${interval / 60000} minutos eu boto pra quebrar!`)
+    ctx.reply('Se preparem para a coluna SUPREMA!');
+    ctx.reply(`em ${interval / 60000} minutos eu boto pra quebrar!`);
     let intervalId = setInterval(async () => {
-      ctx.reply('POSTURA!')
+      ctx.reply('POSTURA!');
     }, interval)
     ctx.session.warning = {
       chatId: ctx.chat.id,
       warningId: intervalId
-    }
+    };
   }
 })
 
 bot.command('abort', (ctx) => {
   if (ctx.session.warning) {
-    ctx.reply('mamae pq me abortastes 😥')
-    clearInterval(ctx.session.warning.warningId)
+    ctx.reply('mamae pq me abortastes 😥');
+    clearInterval(ctx.session.warning.warningId);
   }
 })
 
 bot.command('arrumei', (ctx) => {
-    ctx.reply('ctx.message.from.username!')
+    ctx.reply(`Parabéns ${ctx.message.from.username} você arrumou sou postura, fale com o eduardo para acresentar mais funcoinalidades para a aplicação`);
 })
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.json({message: "hello amigos"})
-})
+  res.json({message: "hello amigos"});
+});
 
 app.listen(PORT, () => {
-  console.log(`Bot running! At the port ${PORT}`)
-  bot.launch()
-})
+  console.log(`Bot running! At the port ${PORT}`);
+  bot.launch();
+});
